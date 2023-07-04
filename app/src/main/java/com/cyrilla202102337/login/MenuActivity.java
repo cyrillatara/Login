bf661dd031d7faeff81a8a921e143e9bccf5fcba
cyrilla <92079267+cyrillatara@users.noreply.github.com>
2023-07-04 11:37:33 +0700

    Initial commit

---
+++ app/src/main/java/com/cyrilla202102337/login/MenuActivity.java
@@ -0,0 +1,14 @@
+package com.cyrilla202102337.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
